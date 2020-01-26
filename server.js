32f3cb8b2088b43e25f67a7a9ad195444087b6e5
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-26 18:13:56 -0500

    Connect app to mongoDb

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const connectDB = require('./config/db.js')
 
+const PORT = process.env.PORT || 5000
 const app = express()
 
+connectDB()
 
 app.use('/api/users', require('./routes/users.js'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/auth', require('./routes/auth'))
 
-const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
