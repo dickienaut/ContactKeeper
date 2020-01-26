e687618163b5210d5fc584c4cbe27d6353c05888
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-26 17:24:59 -0500

    Create skeleton for backend routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 
 const app = express()
+
+
+app.use('/api/users', require('./routes/users.js'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/auth', require('./routes/auth'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
